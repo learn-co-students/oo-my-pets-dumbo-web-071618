1785addd6b6780d0d6f454caa4173b1b104b4aea
personabe1984 <personabe1984@gmail.com>
2018-07-19 14:08:11 -0400

    init commit

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
 class Cat
   # code goes here
+  # attr_accessor
+  # attr_writer
+  # attr_reader
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
 end
